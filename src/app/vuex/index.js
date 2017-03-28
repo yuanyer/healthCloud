3fb49997041dfa97d6777842791dabd2ef2aa26b
zhangyuanyuan031 <zhangyuanyuan031@pingan.com.cn>
2017-03-28 09:40:01 +0800

    all

--- src/app/vuex/index.js
+++ src/app/vuex/index.js
@@ -1,3 +1,1 @@
-import login from './login/index.js';
-import test from './test/index.js';
-export default {...login, ...test}
+import login from './login/index.js';import test from './test/index.js'; export default {...login, ...test}
